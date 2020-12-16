c3ddba06ba9321112795da72cb9328580b788388
tbprojects <info@tbprojects.pl>
2020-12-16 21:54:57 +0100

    upgrade angular

--- src/app/space/space-ship/space-ship.component.spec.ts
+++ src/app/space/space-ship/space-ship.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SpaceShipComponent } from './space-ship.component';
 
@@ -6,7 +6,7 @@ describe('SpaceShipComponent', () => {
   let component: SpaceShipComponent;
   let fixture: ComponentFixture<SpaceShipComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SpaceShipComponent ]
     })
